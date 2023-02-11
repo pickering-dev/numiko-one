31d07cb853b7dba8617ca2bcd599a196ba7073a0
pckrng_vision <joe@visionsharp.co.uk>
2023-02-11 23:43:20 +0000

    scripts styling desktop

---
+++ scripts/scripts.js
@@ -0,0 +1,45 @@
+const numbers = document.querySelectorAll(".number h4");
+const targetValues = [10, 314, 12];
+const duration = 2000; // animation duration in milliseconds
+
+// Step 1: Create a loading screen element
+const loadingScreen = document.createElement("div");
+loadingScreen.id = "loading-screen";
+loadingScreen.textContent = "Loading...";
+document.body.appendChild(loadingScreen);
+
+// Step 2: Show the loading screen
+loadingScreen.style.display = "flex";
+
+// Step 3: Delay the start of the number animation by 3 seconds
+setTimeout(function () {
+	// Step 4: Hide the loading screen
+	loadingScreen.style.display = "none";
+
+	// Step 5: Start the number animation
+	function animateNumbers() {
+		const startTime = new Date().getTime();
+		const updateInterval = 50; // milliseconds
+
+		const timer = setInterval(function () {
+			const timePassed = new Date().getTime() - startTime;
+			const progress = timePassed / duration;
+
+			if (progress >= 1) {
+				clearInterval(timer);
+			}
+
+			numbers.forEach(function (number, index) {
+				const startValue = 0;
+				const endValue = targetValues[index];
+				const currentValue = Math.floor(
+					startValue + (endValue - startValue) * progress
+				);
+
+				number.textContent = currentValue.toLocaleString();
+			});
+		}, updateInterval);
+	}
+
+	animateNumbers();
+}, 3000); // 3000 milliseconds = 3 seconds
