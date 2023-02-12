0e99496b7245f06688686fa375aa63945693c283
pckrng_vision <joe@visionsharp.co.uk>
2023-02-12 13:33:15 +0000

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+Stats info card component (numiko test 1)
+
+<!-- Built With -->
+
+[html]: https://developer.mozilla.org/en-US/docs/Web/HTML
+[sass]: https://sass-lang.com/
+[javascript]: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+<!-- Getting Started -->
+
+Download the project from https://github.com/pickering-dev/numiko-one and open the index.html file in your browser.
+
+<!-- Features -->
+
+- Loading screen
+- Responsive, animated design
+- Semantic HTML
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,24 +2,24 @@ const numbers = document.querySelectorAll(".number h4");
 const targetValues = [10, 314, 12];
 const duration = 2000; // animation duration in milliseconds
 
-// Step 1: Create a loading screen element
+//  Create a loading screen element
 const loadingScreen = document.createElement("div");
 loadingScreen.id = "loading-screen";
 loadingScreen.textContent = "Loading";
 document.body.appendChild(loadingScreen);
 
-// Step 2: Show the loading screen
+// Show the loading screen
 loadingScreen.style.display = "flex";
 loadingScreen.style.opacity = 1;
 loadingScreen.style.visibility = "visible";
 
-// Step 3: Delay the start of the number animation by 3 seconds
+// Delay the start of the number animation by 3 seconds
 setTimeout(function () {
 	// Step 4: Hide the loading screen
 	loadingScreen.style.opacity = 0;
 	loadingScreen.style.visibility = "hidden";
 
-	// Step 5: Start the number animation
+	// Start the number animation
 	function animateNumbers() {
 		const startTime = new Date().getTime();
 		const updateInterval = 50; // milliseconds
@@ -45,4 +45,4 @@ setTimeout(function () {
 	}
 
 	animateNumbers();
-}, 3000); // 3000 milliseconds = 3 seconds
+}, 3000); // 3 seconds
